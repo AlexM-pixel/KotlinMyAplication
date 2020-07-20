5b29440b34839f233dbed07c4f28da63911dcce8
Alex_Manego <aleksandrmanego@gmail.com>
2020-07-20 21:20:02 +0300

    1-я задача(на сортировку)

--- app/src/main/java/com/example/kotlinmyaplicationtasks/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmyaplicationtasks/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.kotlinmyaplicationtasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.list_sortid)
+        var list = listOf("2", "12", "53532", "65656", "131131", "3", "351")
+        list = list.filter { it.length % 2 == 0 }
+        list = list.sortedBy { it.length }
+        textView.text = list.getOrNull(list.lastIndex)?:"Не найдено"
     }
 }
