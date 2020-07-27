9620c5f46fdf11413e1b8f6879dc18de9965fef8
Alex_Manego <aleksandrmanego@gmail.com>
2020-07-27 12:38:58 +0300

    task4beta

---
+++ app/src/main/java/com/example/kotlinmyaplicationtasks/task4/MainActivity4.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinmyaplicationtasks.task4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinmyaplicationtasks.R
+
+class MainActivity4 :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.maintask4layout)
+    }
+}
