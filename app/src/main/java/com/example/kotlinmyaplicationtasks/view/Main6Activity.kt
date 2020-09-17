0f0c3b112fc2e7a2d1cb8ffdf6b1e65d3f11627f
Alex_Manego <aleksandrmanego@gmail.com>
2020-09-17 11:10:23 +0300

    dataBinding with mvvm in mainThread

--- app/src/main/java/com/example/kotlinmyaplicationtasks/view/Main6Activity.kt
+++ app/src/main/java/com/example/kotlinmyaplicationtasks/view/Main6Activity.kt
@@ -1,11 +1,13 @@
-package com.example.kotlinmyaplicationtasks
+package com.example.kotlinmyaplicationtasks.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlinmyaplicationtasks.R
 
-class MainActivity : AppCompatActivity() {
+class Main6Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
